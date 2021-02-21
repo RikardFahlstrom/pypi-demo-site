6f43b101962b5a001662a92369b5f9f9a40525f6
Rikard Fahlstrom <rikard.fahlstrom@gmail.com>
2021-02-21 21:13:40 +0100

    Done with chapter 10

--- pypi_org/data/db_session.py
+++ pypi_org/data/db_session.py
@@ -1,15 +1,16 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
 
 from pypi_org.data.modelbase import SqlAlchemyBase
 
-factory = False
+__factory = False
 
 
 def global_init(db_file: str):
-    global factory
+    global __factory
 
-    if factory:
+    if __factory:
         return
 
     if not db_file or not db_file.strip():
@@ -18,8 +19,13 @@ def global_init(db_file: str):
     conn_str = 'sqlite:///' + db_file.strip()
     print("Connecting to DB with {}".format(conn_str))
 
-    engine = sa.create_engine(conn_str, echo=False)
-    factory = orm.sessionmaker(bind=engine)
+    engine = sa.create_engine(conn_str, echo=True)
+    __factory = orm.sessionmaker(bind=engine)
 
     import pypi_org.data.__all_models
     SqlAlchemyBase.metadata.create_all(engine)
+
+
+def create_session() -> Session:
+    global __factory
+    return __factory()
